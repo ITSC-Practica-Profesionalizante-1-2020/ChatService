7f0dd96ebede664c03bdc679888b8a220c7c6102
Mauricio Gaggero <mauricio.gaggero@hotmail.com>
2020-08-15 17:25:13 -0300

    Preparando la cancha

---
+++ ChatService/Data/DataContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ChatService.Data
+{
+    public class DataContext : DbContext
+    {
+
+        public DataContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Asistencia> Asistencias { get; set; }
+        public DbSet<Participante> Participantes { get; set; }
+        public DbSet<Sala> Salas { get; set; }
+    }
+}
